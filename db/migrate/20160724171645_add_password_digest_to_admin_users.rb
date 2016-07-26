e1eb385d2971ae02a0ab59dafd68f4a18fa6ba16
Saulo Campos <saulocamposi@gmail.com>
2016-07-26 01:17:31 -0300

    Add password digest field migrati

---
+++ db/migrate/20160724171645_add_password_digest_to_admin_users.rb
@@ -0,0 +1,9 @@
+class AddPasswordDigestToAdminUsers < ActiveRecord::Migration
+  def up
+    add_column "admin_users", "password_digest", :string
+  end
+
+  def down
+    remove_column "admin_users", "password_digest"
+  end
+end
